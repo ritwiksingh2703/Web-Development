428089f36c79b4089e31bc5b4e2cc81570b0d2c1
Ritwik Kumar Singh <ritwik2703singh@gmail.com>
2021-02-18 12:53:35 +0530

    Node Callback and promise

--- node mongo/index.js
+++ node mongo/index.js
@@ -4,35 +4,37 @@ const dboper=require('./operations');
 const url='mongodb://localhost:27017/';
 const dbname="conFusion";
 
-MongoClient.connect(url, (err, client) => {
+MongoClient.connect(url).then((client) =>{
 
-    assert.equal(err,null);
-
+    
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
     dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
-        "dishes", (result) => {
+        "dishes")
+        .then((result) =>{
             console.log("Insert Document:\n", result.ops);
 
-            dboper.findDocuments(db, "dishes", (docs) => {
-                console.log("Found Documents:\n", docs);
+            dboper.findDocuments(db, "dishes")
+                
+        })
+        .then((docs) =>{
+            console.log('Found Documents \n',docs);
 
-                dboper.updateDocument(db, { name: "Vadonut" },
-                    { description: "Updated Test" }, "dishes",
-                    (result) => {
-                        console.log("Updated Document:\n", result.result);
+            return db.dropCollection('dishes');
+        })
+        .then((result) => {
+                
+            console.log('Dropped Collection: ', result);
 
-                        dboper.findDocuments(db, "dishes", (docs) => {
-                            console.log("Found Updated Documents:\n", docs);
-                            
-                            db.dropCollection("dishes", (result) => {
-                                console.log("Dropped Collection: ", result);
+            return client.close();
 
-                                client.close();
-                            });
-                        });
-                    });
-            });
-    });
+        })
+        .catch((err) => console.log(err));
 });
+                            
+                    
+                  
+          
+
+    
--- node mongo/operations.js
+++ node mongo/operations.js
@@ -1,37 +1,23 @@
 const assert = require('assert');
 
-exports.insertDocument = (db, document, collection, callback) => {
+exports.insertDocument = (db, document, collection,callback) =>{
     const coll = db.collection(collection);
-    coll.insert(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Inserted " + result.result.n +
-            " documents into the collection " + collection);
-        callback(result);
-    });
+    return coll.insert(document);
 };
 
 exports.findDocuments = (db, collection, callback) => {
     const coll = db.collection(collection);
-    coll.find({}).toArray((err, docs) => {
-        assert.equal(err, null);
-        callback(docs);        
-    });
+    coll.find({}).toArray();
 };
 
 exports.removeDocument = (db, document, collection, callback) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.equal(err, null);
-        console.log("Removed the document ", document);
-        callback(result);        
-    });
-};
+    coll.deleteOne(document);
+    
+    }
 
 exports.updateDocument = (db, document, update, collection, callback) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.equal(err, null);
-        console.log("Updated the document with ", update);
-        callback(result);        
-    });
+    coll.updateOne(document, { $set: update }, null)
+
 };
